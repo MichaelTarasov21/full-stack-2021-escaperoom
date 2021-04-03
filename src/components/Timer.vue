f21a760db3c1fc306b5ce69eefc2537090279c0c
Michael Tarasov <Michael.Tarasov@tutanota.com>
2021-04-03 14:37:33 -0400

    Made score only save once
    Signed-off-by: Michael Tarasov <Michael.Tarasov@tutanota.com>

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -7,16 +7,24 @@
 	import firebase from "firebase/app";
 	export default {
 		emits: ["Gameover"],
-		props: {Gamewon: Boolean},
+		props: { Gamewon: Boolean },
 		data() {
 			return { remainingtime: 1200 };
 		},
 		mixins: [VueTimers],
 		methods: {
 			tickdown: function() {
-				if (this.Gamewon){
+				if (this.Gamewon) {
 					this.$timer.stop("tickdown");
-					this.storetime(this.remainingtime)
+					firebase.database().ref().child("Users").child(firebase.auth().currentUser.uid).get().then(function(snapshot) {
+							const FalseStart = snapshot.val().FalseStart;
+							console.log(FalseStart);
+							if (!FalseStart) {
+								this.storetime(this.remainingtime);
+							} else {
+								alert("It appers that you have played the game before. Your score will not be saved in the leaderboard");
+							}
+						});
 				}
 				this.remainingtime = this.remainingtime - 1;
 				if (this.remainingtime === 0) {
@@ -44,61 +52,64 @@
 					const username = user.displayName;
 					const yourtime = this.remainingtime;
 					if (remoteleaderboard[4].time > yourtime && yourtime > remoteleaderboard[5].time) {
-						let newleaderboard = remoteleaderboard
-						newleaderboard[5].name = username
-						newleaderboard[5].time = yourtime
-						this.writedata(newleaderboard, [5])
+						let newleaderboard = remoteleaderboard;
+						newleaderboard[5].name = username;
+						newleaderboard[5].time = yourtime;
+						this.writedata(newleaderboard, [5]);
 					} else if (remoteleaderboard[3].time > yourtime && yourtime > remoteleaderboard[4].time) {
-						let newleaderboard = remoteleaderboard
-						this.shiftdata(newleaderboard,4,5)
-						newleaderboard[4].name = username
-						newleaderboard[4].time = yourtime
-						this.writedata(newleaderboard, [4,5])
+						let newleaderboard = remoteleaderboard;
+						this.shiftdata(newleaderboard, 4, 5);
+						newleaderboard[4].name = username;
+						newleaderboard[4].time = yourtime;
+						this.writedata(newleaderboard, [4, 5]);
 					} else if (remoteleaderboard[2].time > yourtime && yourtime > remoteleaderboard[3].time) {
-						let newleaderboard = remoteleaderboard
-						this.shiftdata(newleaderboard,4,5)
-						this.shiftdata(newleaderboard,3,4)
-						newleaderboard[3].name = username
-						newleaderboard[3].time = yourtime
-						this.writedata(newleaderboard, [3,4,5])
+						let newleaderboard = remoteleaderboard;
+						this.shiftdata(newleaderboard, 4, 5);
+						this.shiftdata(newleaderboard, 3, 4);
+						newleaderboard[3].name = username;
+						newleaderboard[3].time = yourtime;
+						this.writedata(newleaderboard, [3, 4, 5]);
 					} else if (remoteleaderboard[1].time > yourtime && yourtime > remoteleaderboard[2].time) {
-						let newleaderboard = remoteleaderboard
-						this.shiftdata(newleaderboard,4,5)
-						this.shiftdata(newleaderboard,3,4)
-						this.shiftdata(newleaderboard,2,3)
-						newleaderboard[2].name = username
-						newleaderboard[2].time = yourtime
-						this.writedata(newleaderboard, [2,3,4,5])
+						let newleaderboard = remoteleaderboard;
+						this.shiftdata(newleaderboard, 4, 5);
+						this.shiftdata(newleaderboard, 3, 4);
+						this.shiftdata(newleaderboard, 2, 3);
+						newleaderboard[2].name = username;
+						newleaderboard[2].time = yourtime;
+						this.writedata(newleaderboard, [2, 3, 4, 5]);
 					} else {
-						let newleaderboard = remoteleaderboard
-						this.shiftdata(newleaderboard,4,5)
-						this.shiftdata(newleaderboard,3,4)
-						this.shiftdata(newleaderboard,2,3)
-						this.shiftdata(newleaderboard,1,2)
-						newleaderboard[1].name = username
-						newleaderboard[1].time = yourtime
-						this.writedata(newleaderboard, [1,2,3,4,5])
+						let newleaderboard = remoteleaderboard;
+						this.shiftdata(newleaderboard, 4, 5);
+						this.shiftdata(newleaderboard, 3, 4);
+						this.shiftdata(newleaderboard, 2, 3);
+						this.shiftdata(newleaderboard, 1, 2);
+						newleaderboard[1].name = username;
+						newleaderboard[1].time = yourtime;
+						this.writedata(newleaderboard, [1, 2, 3, 4, 5]);
 					}
 					console.log("You are on the leaderboard :)");
 				}
 			},
-			shiftdata: function(dataset, from, to){
-				dataset[to].name = dataset[from].name
-				dataset[to].time = dataset[from].time
+			shiftdata: function(dataset, from, to) {
+				dataset[to].name = dataset[from].name;
+				dataset[to].time = dataset[from].time;
 			},
-			writedata: function(data, changes){
-				changes.forEach(element => {
+			writedata: function(data, changes) {
+				changes.forEach((element) => {
 					let update = {};
-					update['/Leaderboard/' + element + '/'] = data[element]
-					return firebase.database().ref().update(update);
+					update["/Leaderboard/" + element + "/"] = data[element];
+					return firebase
+						.database()
+						.ref()
+						.update(update);
 				});
-			}
+			},
 		},
 		timers: { tickdown: { time: 1000, autostart: true, repeat: true } }, //when using disable autostart and make a button with function this.$timer.start('tickdown')
 	};
 </script>
 <style scoped>
-	button{
+	button {
 		position: absolute;
 		left: 0%;
 		bottom: 0%;
