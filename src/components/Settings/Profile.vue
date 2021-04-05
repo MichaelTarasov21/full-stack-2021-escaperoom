0780ecc9e3b70ae623388090c2e8576592088543
Michael Tarasov <Michael.Tarasov@tutanota.com>
2021-04-05 16:52:28 -0400

    Rewrote Profile page to work with Firebase
    Signed-off-by: Michael Tarasov <Michael.Tarasov@tutanota.com>

--- src/components/Settings/Profile.vue
+++ src/components/Settings/Profile.vue
@@ -1,55 +1,52 @@
 <template>
-  <div>
-    <h1 class="profile">Profile</h1>
-    <div class="profile-info">
-      <h3 id="name"></h3>
-      <h3 id="email"></h3>
-      <h3 id="room1" ></h3>
-      <h3 id="room2" ></h3>
-      <h3 id="room3" ></h3>
-      <h3 id="room4" ></h3>
-      
-    </div>
-    <!-- <h3 id="joinDate" > </h3> -->
-  </div>
+	<div>
+		<h1 class="profile">Profile</h1>
+		<div class="profile-info">
+			<h3>Name: {{ name }}</h3>
+			<h3>Email: {{ email }}</h3>
+			<h3>Run# {{ run }}</h3>
+			<h3>Room One Time: {{ pad2(Math.floor(data[run].RoomOne / 60)) + ":" + pad2(Math.floor(data[run].RoomOne % 60)) }}</h3>
+			<h3>Room Two Time: {{ pad2(Math.floor(data[run].RoomTwo / 60)) + ":" + pad2(Math.floor(data[run].RoomTwo % 60)) }}</h3>
+			<h3>Room Three Time: {{ pad2(Math.floor(data[run].RoomThree / 60)) + ":" + pad2(Math.floor(data[run].RoomThree % 60)) }}</h3>
+			<h3>Room Four Time: {{ pad2(Math.floor(data[run].RoomFour / 60)) + ":" + pad2(Math.floor(data[run].RoomFour % 60)) }}</h3>
+		</div>
+	</div>
 </template>
 
 <script>
-import firebase from "firebase/app";
-export default {
-  name: "Profile",
-  props: {
-    msg: String,
-  },
-  methods: {
-    checkLogIn: function () {
-      firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-          const name = document.querySelector("#name");
-          name.innerHTML = `Logged in as: ${user.displayName}`;
-
-          const email = document.querySelector("#email");
-          email.innerHTML = `Email: ${user.email}`;
-
-          const room1 = document.querySelector("#room1");
-          room1.innerHTML = `Email: ${user.email}`;
-        }
-      });
-    },
-  },
-
-  created: function () {
-    this.checkLogIn();
-  },
-};
+	import firebase from "firebase/app";
+	export default {
+		name: "Profile",
+		data() {
+			return {
+				name: null,
+				email: null,
+				run: 1,
+				data: null,
+			};
+		},
+		methods: {
+			pad2: function(number) {
+				return (number < 10 ? "0" : "") + number;
+			},
+		},
+		created: function() {
+			this.name = firebase.auth().currentUser.displayName;
+			this.email = firebase.auth().currentUser.email;
+			const request = firebase.database().ref().child("Users").child(firebase.auth().currentUser.uid).child("Runs");
+			request.on("value", (snapshot) => {
+				this.data = snapshot.val();
+			});
+		},
+	};
 </script>
 
-<style >
-.profile {
-  background-color: yellow;
-}
+<style>
+	.profile {
+		background-color: yellow;
+	}
 
-.profile-info {
-  text-align: left;
-}
+	.profile-info {
+		text-align: left;
+	}
 </style>
