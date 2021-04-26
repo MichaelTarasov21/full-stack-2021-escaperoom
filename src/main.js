5c3d312ac52537bfb1c232ccd817628a43672bb3
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2021-04-26 09:31:40 -0400

    Added .env file support

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import App from './App.vue'
 // };
 
 var firebaseConfig = {
-  apiKey: 'AIzaSyAMHt38VEtOh7ggYRQLQooixyo08wxUhGc',
+  apiKey: process.env.VUE_APP_API_KEY,
   authDomain: "escape-room-project-677cd.firebaseapp.com",
   projectId: "escape-room-project-677cd",
   storageBucket: "escape-room-project-677cd.appspot.com",
