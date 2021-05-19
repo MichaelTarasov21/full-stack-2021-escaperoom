7d18baf0eb9a82968b781ba0334a3a33d4a5e858
Michael Tarasov <Michael.Tarasov@tutanota.com>
2021-05-19 19:08:09 -0400

    Added Enemy movement and the ability for the player to lost the minigame.
    Signed-off-by: Michael Tarasov <Michael.Tarasov@tutanota.com>

---
+++ src/components/RoomFour/ArcadeWon.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        You are winner
+        <!-- This is a stub. It can not be rendered naturaly yet so I will not build it yet -->
+    </div>
+</template>
--- src/components/RoomFour/WallBreaker.vue
+++ src/components/RoomFour/WallBreaker.vue
@@ -1,5 +1,7 @@
 <template>
-	<div class="screen">
+	<ArcadeWon v-if="Won" @ShowHint="ShowHint()" />
+	<ArcadeLost v-else-if="Lost" @Restart="Reset" />
+	<div v-else class="screen">
 		<div id="enemies">
 			<div id="row1" v-bind:style="EnemyR1">
 				<img v-if="Enemies[0]" v-bind:style="EnemyC1" class="enemy" width="50px" height="40px" src="@/assets/Images/Blue_Space_Invader.gif" />
@@ -49,10 +51,16 @@
 	</div>
 </template>
 <script>
+	import ArcadeWon from "./ArcadeWon";
+	import ArcadeLost from "./ArcadeLost";
 	import { mixin as VueTimers } from "vue-timers";
 	export default {
 		name: "WallBreaker",
 		emits: ["Minigamewon"],
+		components: {
+			ArcadeWon,
+			ArcadeLost,
+		},
 		mixins: [VueTimers],
 		data() {
 			return {
@@ -62,7 +70,7 @@
 					left: "",
 				},
 				EnemyC1: {
-					left: "0px",
+					left: "1px",
 				},
 				EnemyC2: {
 					left: "70px",
@@ -95,12 +103,14 @@
 					top: "160px",
 				},
 				Enemies: [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true],
-				Minigamewon: false,
+				EnemySpeed: 1,
+				EnemyVelocity: 2,
+				Lost: false,
+				Won: false,
 			};
 		},
 		mounted() {
-			this.Playerstyle.left = ((((screen.width - 128) * .7) / 2) - 35) + "px";
-			console.log(this.Screen);
+			this.Playerstyle.left = ((screen.width - 128) * 0.7) / 2 - 35 + "px";
 			window.addEventListener("keydown", function(event) {
 				const buttons = document.getElementsByClassName("button");
 				if (event.key === "ArrowLeft") {
@@ -125,22 +135,137 @@
 			});
 		},
 		methods: {
+			Reset: function() {
+				this.Playerstyle.left = ((screen.width - 128) * 0.7) / 2 - 35 + "px";
+				this.EnemyC1.left = "1px";
+				this.EnemyC2.left = "70px";
+				this.EnemyC3.left = "140px";
+				this.EnemyC4.left = "210px";
+				this.EnemyC5.left = "280px";
+				this.EnemyC6.left = "350px";
+				this.EnemyR1.top = "0px";
+				this.EnemyR2.top = "40px";
+				this.EnemyR3.top = "80px";
+				this.EnemyR4.top = "120px";
+				this.EnemyR5.top = "160px";
+				this.Enemies = [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true];
+				this.EnemySpeed = 1;
+				this.EnemyVelocity = 2;
+				this.Lost = false;
+				this.$timer.start("AlienMove");
+			},
 			HeroMove: function(velocity) {
 				let move = parseInt(this.Playerstyle.left);
 				move = move + velocity;
-				if (move > (screen.width - 128) * .7) {
-					move = (screen.width - 128) * .7;
+				if (move > (screen.width - 128) * 0.7) {
+					move = (screen.width - 128) * 0.7;
 				} else if (move < 0) {
 					move = 0;
 				}
 				move = move + "px";
 				this.Playerstyle.left = move;
 			},
-			DisplayClue() {
+			AlienMove: function() {
+				function move(item, amount) {
+					let move = parseInt(item);
+					move = move + amount;
+					move = move + "px";
+					return move;
+				}
+				const C1 = parseInt(this.EnemyC1.left);
+				const C2 = parseInt(this.EnemyC2.left);
+				const C3 = parseInt(this.EnemyC3.left);
+				const C4 = parseInt(this.EnemyC4.left);
+				const C5 = parseInt(this.EnemyC5.left);
+				const C6 = parseInt(this.EnemyC6.left);
+				let leftrow;
+				let rightrow;
+				const C1Active = this.Enemies[0] + this.Enemies[6] + this.Enemies[12] + this.Enemies[18] + this.Enemies[24];
+				const C2Active = this.Enemies[1] + this.Enemies[7] + this.Enemies[13] + this.Enemies[19] + this.Enemies[25];
+				const C3Active = this.Enemies[2] + this.Enemies[8] + this.Enemies[14] + this.Enemies[20] + this.Enemies[26];
+				const C4Active = this.Enemies[3] + this.Enemies[9] + this.Enemies[15] + this.Enemies[21] + this.Enemies[27];
+				const C5Active = this.Enemies[4] + this.Enemies[10] + this.Enemies[16] + this.Enemies[22] + this.Enemies[28];
+				const C6Active = this.Enemies[5] + this.Enemies[11] + this.Enemies[17] + this.Enemies[23] + this.Enemies[29];
+				if (!C1Active && !C2Active && !C3Active && !C4Active && !C5Active && !C6Active) {
+					this.$timer.stop("AlienMove");
+					this.Won = true;
+					return;
+				} //move this to the bullet section
+				if (C1Active) {
+					leftrow = C1;
+				} else if (C2Active) {
+					leftrow = C2;
+				} else if (C3Active) {
+					leftrow = C3;
+				} else if (C4Active) {
+					leftrow = C4;
+				} else if (C5Active) {
+					leftrow = C5;
+				} else if (C6Active) {
+					leftrow = C6;
+				}
+				if (C6Active) {
+					rightrow = C6;
+				} else if (C5Active) {
+					rightrow = C5;
+				} else if (C4Active) {
+					rightrow = C4;
+				} else if (C3Active) {
+					rightrow = C3;
+				} else if (C2Active) {
+					rightrow = C2;
+				} else if (C1Active) {
+					rightrow = C1;
+				}
+				if (rightrow + this.EnemySpeed * this.EnemyVelocity >= (screen.width - 128) * 0.7 || leftrow + this.EnemySpeed * this.EnemyVelocity <= 0) {
+					console.log(screen);
+					this.EnemyVelocity = this.EnemyVelocity * -1;
+					const R1 = this.EnemyR1.top;
+					const R2 = this.EnemyR2.top;
+					const R3 = this.EnemyR3.top;
+					const R4 = this.EnemyR4.top;
+					const R5 = this.EnemyR5.top;
+					this.EnemyR1.top = move(R1, 30);
+					this.EnemyR2.top = move(R2, 30);
+					this.EnemyR3.top = move(R3, 30);
+					this.EnemyR4.top = move(R4, 30);
+					this.EnemyR5.top = move(R5, 30);
+					let LowestRow;
+					const R1Active = this.Enemies[0] + this.Enemies[1] + this.Enemies[2] + this.Enemies[3] + this.Enemies[4] + this.Enemies[5];
+					const R2Active = this.Enemies[6] + this.Enemies[7] + this.Enemies[8] + this.Enemies[9] + this.Enemies[10] + this.Enemies[11];
+					const R3Active = this.Enemies[12] + this.Enemies[13] + this.Enemies[14] + this.Enemies[15] + this.Enemies[16] + this.Enemies[17];
+					const R4Active = this.Enemies[18] + this.Enemies[19] + this.Enemies[20] + this.Enemies[21] + this.Enemies[22] + this.Enemies[23];
+					const R5Active = this.Enemies[24] + this.Enemies[25] + this.Enemies[26] + this.Enemies[27] + this.Enemies[28] + this.Enemies[29];
+					if (R5Active) {
+						LowestRow = this.EnemyR5.top;
+					} else if (R4Active) {
+						LowestRow = this.EnemyR4.top;
+					} else if (R3Active) {
+						LowestRow = this.EnemyR3.top;
+					} else if (R2Active) {
+						LowestRow = this.EnemyR2.top;
+					} else if (R1Active) {
+						LowestRow = this.EnemyR1.top;
+					}
+					if (parseInt(LowestRow) >= (screen.availHeight - 128) * 0.7 - 40) {
+						console.log("You are loserman");
+						this.$timer.stop("AlienMove");
+						this.Lost = true;
+					}
+				}
+				const speed = this.EnemySpeed * this.EnemyVelocity;
+				this.EnemyC1.left = move(C1, speed);
+				this.EnemyC2.left = move(C2, speed);
+				this.EnemyC3.left = move(C3, speed);
+				this.EnemyC4.left = move(C4, speed);
+				this.EnemyC5.left = move(C5, speed);
+				this.EnemyC6.left = move(C6, speed);
+			},
+			ShowHint: function() {
 				this.$emit("Minigamewon");
 			},
 		},
-		//		timers: { ListenToEvent: {time: 1000, autostart: true, repeat: true }},
+		timers: { AlienMove: { time: 40, autostart: true, repeat: true } }, // 40 milliseconds seems to be the lowest time interval at which the movement of the aliens still appears smoothe
 	};
 </script>
 <style scoped>
