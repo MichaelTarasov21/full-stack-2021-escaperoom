f5a3897145f4f9b9b4ca4ac87a669d05e09a04bf
TurtleSun <57500228+TurtleSun@users.noreply.github.com>
2021-05-23 22:08:19 -0400

    .

--- src/components/RoomThree/RoomThree.vue
+++ src/components/RoomThree/RoomThree.vue
@@ -65,6 +65,123 @@ export default {
     this.coordinates();
     //this.walls();
     this.addToInventory();
+  
+  const sprite = document.getElementById("spriteCharacter");
+    const character = document.getElementById("character");
+    // upKey = document.getElementById("upKey");
+    // leftKey = document.getElementById("left");
+    // downKey = document.getElementById("down");
+    // rightKey = document.getElementById("right");
+    let screenWidth;
+    let screenHeight;
+
+    let x = 0;
+    let y = 0;
+
+    //screen size
+    const findScreenSize = function() {
+      console.log("resize");
+      //divide by 6 to put in rem
+      //6 bc width of sprite is 6
+      screenWidth = window.innerWidth / 16 - 6;
+      document.getElementById("mapItems").style.width = screenWidth + "rem";
+      //8 bc height of sprite is 8
+      //plus 8 for inventory
+      screenHeight = window.innerHeight / 16 - 14;
+      document.getElementById("mapItems").style.height = screenHeight + "rem";
+    };
+
+    findScreenSize();
+    window.onresize = findScreenSize;
+
+    //animation
+    const addAnimation = function() {
+      sprite.classList.add("Character_animation");
+    };
+
+    const removeAnimation = function() {
+      sprite.classList.remove("Character_animation");
+    };
+
+    //movement
+    const moveUp = function() {
+      console.log("Up key is connected");
+      sprite.classList.add("face-up");
+      sprite.classList.remove("face-down", "face-right", "face-left");
+      y = y - 1;
+    };
+
+    const moveRight = function() {
+      console.log("Right key is connected");
+      sprite.classList.add("face-right");
+      sprite.classList.remove("face-down", "face-left", "face-up");
+      x = x + 1;
+      console.log(x);
+    };
+
+    const moveLeft = function() {
+      console.log("Left key is connected");
+      sprite.classList.add("face-left");
+      sprite.classList.remove("face-down", "face-right", "face-up");
+      x = x - 1;
+    };
+
+    const moveDown = function() {
+      console.log("Down key is connected");
+      sprite.classList.add("face-down");
+      sprite.classList.remove("face-right", "face-left", "face-up");
+      y = y + 1;
+    };
+
+    const updateSpritePosition = function() {
+      character.style.left = x + "rem";
+      character.style.top = y + "rem";
+    };
+
+    //boundaries
+    const walls = function() {
+      if (x < 0) {
+        sprite.classList.remove("Character_animation");
+        x = 0;
+      } else if (x > screenWidth) {
+        sprite.classList.remove("Character_animation");
+        x = screenWidth;
+      } else if (y < 0) {
+        sprite.classList.remove("Character_animation");
+        y = 0;
+      } else if (y > screenHeight) {
+        sprite.classList.remove("Character_animation");
+        y = screenHeight;
+      }
+    };
+
+    //keyboard
+    const keyboardAnimate = function() {
+      if (event.keyCode == "38" || event.keyCode == "87") {
+        moveUp();
+      } else if (event.keyCode == "39" || event.keyCode == "68") {
+        moveRight();
+      } else if (event.keyCode == "37" || event.keyCode == "65") {
+        moveLeft();
+      } else if (event.keyCode == "40" || event.keyCode == "83") {
+        moveDown();
+      }
+    };
+
+    document.addEventListener("keydown", function() {
+      addAnimation();
+      //animate
+      keyboardAnimate();
+      //check if overboard
+      walls();
+      updateSpritePosition();
+    });
+
+    //when arrow keys are released, stops animation
+    document.addEventListener("keyup", function() {
+      removeAnimation();
+      clearInterval();
+    });
   },
   //props: {keyUpStart:Boolean},
   methods:{
