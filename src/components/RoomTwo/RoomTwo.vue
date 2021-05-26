9bd9cd248c415aea358adc882d58383e704541b7
Michael Tarasov <Michael.Tarasov@tutanota.com>
2021-05-26 15:58:02 -0400

    Rehosted character locally
    Signed-off-by: Michael Tarasov <Michael.Tarasov@tutanota.com>

--- src/components/RoomTwo/RoomTwo.vue
+++ src/components/RoomTwo/RoomTwo.vue
@@ -11,14 +11,14 @@
       <div id="character">
         <img
           class="Character_shadow pixelart"
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacterShadow.png"
+          src="@/assets/Images/Character_Shadow.png"
           alt="Shadow"
         />
 
         <img
           class="Character_spritesheet pixelart face-down"
           id="spriteCharacter"
-          src=" https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacter.png"
+          src="@/assets/Images/Character.png"
           alt="Character"
         />
       </div>
