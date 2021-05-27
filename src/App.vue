b679d52bf24d39eb95b3c9fa6ef6bc6eaf9de244
Michael Tarasov <Michael.Tarasov@tutanota.com>
2021-05-27 17:42:38 -0400

    Rehosted Fonts locally
    Signed-off-by: Michael Tarasov <Michael.Tarasov@tutanota.com>

--- src/App.vue
+++ src/App.vue
@@ -101,13 +101,41 @@
 
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Mystery+Quest&display=swap");
-  @import url("https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap");
-
   @font-face {
     font-family: "analog";
-    src: url("assets/Fonts/timer.woff2") format("woff2"), url("assets/Fonts/timer.woff") format("woff");
+    src: url("~@/assets/Fonts/timer.woff2") format("woff2"), url("assets/Fonts/timer.woff") format("woff");
     font-weight: normal;
     font-style: normal;
+  }
+  @font-face {
+    font-family: 'Roboto Mono';
+    font-style: normal;
+    font-weight: 400;
+    src: url("~@/assets/Fonts/Roboto_Mono_Latin-ext.woff2") format('woff2');
+    unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+  }
+  @font-face {
+    font-family: 'Roboto Mono';
+    font-style: normal;
+    font-weight: 400;
+    src: url("~@/assets/Fonts/Roboto_Mono_Latin.woff2") format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;  
+  }
+  @font-face {
+  font-family: 'Mystery Quest';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url("~@/assets/Fonts/Mystery_Quest_Latin-ext.woff2") format('woff2');
+  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+  }
+  @font-face {
+  font-family: 'Mystery Quest';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url("~@/assets/Fonts/Mystery_Quest_Latin.woff2") format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
   .tab-bkg {
     width: 100%;
